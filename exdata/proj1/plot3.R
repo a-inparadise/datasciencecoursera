fa2f08e81c9f0eb94b3aecee3259ef5abfa55305
Aaron Johnson <aaron.c.johnson@gmail.com>
2014-09-05 16:49:00 -0400

    Re-added files, moved into proj1 folder

---
+++ exdata/proj1/plot3.R
@@ -0,0 +1,23 @@
+# only read in the data that we're lookging for
+# Feb 1, 2007 to Feb 2, 2007
+data <- read.table("household_power_consumption.txt", sep=";",stringsAsFactors=FALSE, header=TRUE, skip=66636, nrows=2880)
+
+# assign column names
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# combine the date and the time
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+# open png stream
+png("plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
+
+# create the line graph
+# global active power along they and date time over x
+par(mfrow=c(1,1)) # reset plot canvas
+with(data, plot(DateTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab=""))
+with(data, points(DateTime, Sub_metering_2, col="red", type="l"))
+with(data, points(DateTime, Sub_metering_3, col="blue", type="l"))
+legend("topright", lwd = 1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# close the stream
+dev.off()
